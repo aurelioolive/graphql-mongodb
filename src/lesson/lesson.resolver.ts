a55e1bda8b3a6174a6006e79018241fe23357453
Aurélio Henrique <aurelio.oliveira@apinteractive.com.br>
2020-06-21 00:23:24 -0300

    created method to get a lession with id

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -9,13 +9,10 @@ export class LessonResolver {
     ){}
 
     @Query(returns => LessonType)
-    lesson() {
-        return {
-            id: '81y3',
-            name: 'Physics Class',
-            startDate: (new Date()).toISOString(),
-            endDate: (new Date()).toISOString(),
-        }
+    lesson(
+        @Args('id') id:string,
+    ) {
+        return this.lessonService.getLesson(id);
     }
 
     @Mutation(returns => LessonType)
