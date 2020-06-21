4f850c375c35059dc09039f38e0396a18e87eb03
Aurélio Henrique <aurelio.oliveira@apinteractive.com.br>
2020-06-20 22:59:09 -0300

    created lesson resolver

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,16 @@
+import { Resolver, Query } from "@nestjs/graphql";
+import { LessonType } from './lesson.type';
+
+@Resolver(of => LessonType)
+export class LessonResolver {
+    
+    @Query(returns => LessonType)
+    lesson() {
+        return {
+            id: '81y3',
+            name: 'Physics Class',
+            startDate: (new Date()).toISOString(),
+            endDate: (new Date()).toISOString(),
+        }
+    }
+}
