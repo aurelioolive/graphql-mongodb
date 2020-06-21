a6cb630ef11e2da8cac458fc52ca51b32b40fdfd
Aurélio Henrique <aurelio.oliveira@apinteractive.com.br>
2020-06-20 22:42:20 -0300

    created lesson module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql'
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,    
-    })
+    }),
+    LessonModule
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+``
